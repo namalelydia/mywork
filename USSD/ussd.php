57963ee0b252c2b2429e5dd780a52078c181661b
namalelydia <lnamale88@gmail.com>
2021-07-22 14:39:07 +0300

    working with USSD

---
+++ USSD/ussd.php
@@ -0,0 +1,15 @@
+<?php
+
+header("content_type:text/plane");
+
+$phone_number = $_POST['phone-number'];
+
+$textfromuser = $_POST['text'];
+
+$sessionID = $_POST['session'];
+
+$servicecode =$_post['servicecode'];
+
+echo"CON welcome to the climate portal";
+
+
--- php-mysqli.php
+++ php-mysqli.php
@@ -1,14 +1,52 @@
 <?php
 
-$mysqli-connection = new mysqli("localhost","root","");
+$host="localhost";
+$username="root";
+$password="";
 
-$sql string= $mysqli-connection ->query("CREATE DATABASE internship");
+$mysqli_connection = new mysqli("localhost","root","");
 
-if ($sql string ){
+$mysqli_connection->query("CREATE  DATABASE IF NOT EXISTS nema_project");
 
-echo ("created DATABASE");
+if($mysqli_connection){
+
+echo "database created";
 
 }else{
 
-	echo $mysqli-connection->error;
+
+	echo $mysqli_connection->error;
+
+
 }
+
+$mysqli_connection->query("CREATE DATABASE nema_project");
+
+$result = mysqli_select_db($mysqli_connection,nema_project);
+
+if($result){
+
+	echo "database selected"; 
+
+}else{
+
+	echo "no selected database;
+}
+
+
+ $mysqli_connection->query("CREATE TABLE IF NOT EXISTS district(
+   DISTRICTID INT(11) NOT NULL AUTO_INCREMENT,PRIMARY KEY(DISTRICT-ID),
+   DISTRICT-NAME VARCHAR(20) NOT NULL,UNIQUE);
+   
+
+
+
+
+
+
+
+
+
+
+
+
